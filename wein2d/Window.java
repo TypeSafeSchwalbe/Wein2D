54e07b295fad480c56c2df67318ab8349d791010
WeinTaubeDev <78918080+WeinTaubeDev@users.noreply.github.com>
2021-08-29 18:32:46 +0200

    Bugfix for drawSprite method

--- wein2d/Window.java
+++ wein2d/Window.java
@@ -164,7 +164,7 @@ public class Window extends JFrame {
   public void drawSprite(Sprite sprite, int posX, int posY, int sizeX, int sizeY, int sPosX, int sPosY, int sSizeX, int sSizeY) {
     int drawObjectsLength = panel.drawObjects.length;
     panel.drawObjects = Arrays.copyOf(panel.drawObjects, drawObjectsLength + 1);
-    panel.drawObjects[drawObjectsLength] = new DrawObject(4, sprite.getImage(), posX, posY, posX + sizeX, posY + sizeY, sPosX, sPosY, sSizeX, sSizeY);
+    panel.drawObjects[drawObjectsLength] = new DrawObject(4, sprite.getImage(), posX, posY, posX + sizeX, posY + sizeY, sPosX, sPosY, sPosX + sSizeX, sPosX + sSizeY);
   }
   public void drawText(String content, int posX, int posY, int size, String fontFamily, int colorR, int colorG, int colorB) {
     int drawObjectsLength = panel.drawObjects.length;
