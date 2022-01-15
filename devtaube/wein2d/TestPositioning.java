bf09c71ff76503b295262b864f9665049d630414
DevTaube <78918080+DevTaube@users.noreply.github.com>
2022-01-15 16:14:19 +0100

    Delete devtaube/wein2d directory

--- devtaube/wein2d/TestPositioning.java
+++
@@ -1,8 +0,0 @@
-package devtaube.wein2d;
-
-public class TestPositioning
-{
-    public static final int LEFT = 0;
-    public static final int CENTER = 1;
-    public static final int RIGHT = 2;
-}
