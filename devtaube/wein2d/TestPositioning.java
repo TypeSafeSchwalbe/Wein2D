c9b2df2e08cda097ae6fbd356292a48bbb8b0ec2
DevTaube <78918080+DevTaube@users.noreply.github.com>
2022-01-15 16:05:09 +0100

    Add files via upload

---
+++ devtaube/wein2d/TestPositioning.java
@@ -0,0 +1,8 @@
+package devtaube.wein2d;
+
+public class TestPositioning
+{
+    public static final int LEFT = 0;
+    public static final int CENTER = 1;
+    public static final int RIGHT = 2;
+}
