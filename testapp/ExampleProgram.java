2a0ef914b1683d66ca926bfa1c8be4575b56281d
DevTaube <78918080+DevTaube@users.noreply.github.com>
2022-01-15 15:28:41 +0100

    Update ExampleProgram.java

--- testapp/ExampleProgram.java
+++ testapp/ExampleProgram.java
@@ -14,18 +14,17 @@ public class ExampleProgram implements Gameloop
 
     public ExampleProgram() // constructor; gets executed on launch
     {
-        window = new Window(848, 480); // create window
-        window.addGameloop(this); // add this as a gameloop (it implements Gameloop)
-        window.setVisible(true); // set the window to visible
-        window.startGameloop(); // start calling 'onFrame()' once per frame
+        window = new Window() // create window object
+                .setGameloopObject(this) // add this as a the gameloop object (it implements Gameloop)
+                .build(); // build the window start calling 'onFrame()' once per frame
     }
 
     public void onFrame() // gets called once per frame
     {
         ballX += 3; // move the ball
-        if (ballX > 848) ballX = -50; // teleport it to the other side if it hits the edge
+        if (ballX > window.width) ballX = -50; // teleport it to the other side if it hits the edge
         window.fill(255, 255, 255); // fill the screen with white
-        window.drawOval(ballX, 215, 50, 50, 59, 187, 164); // draw the ball
+        window.drawOval(ballX, (window.height - 50) / 2, 50, 50, 59, 187, 164); // draw the ball
         window.redraw(); // let the window do it's drawing
     }
 }
