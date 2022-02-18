7f0c93c4e25ca3918c230b6760db476d7ac6732f
DevTaube <78918080+DevTaube@users.noreply.github.com>
2022-02-18 14:27:03 +0100

    Add files via upload

---
+++ devtaube/wein2d/Canvas.java
@@ -0,0 +1,102 @@
+package devtaube.wein2d;
+
+import java.awt.*;
+import javax.swing.*;
+
+class Canvas extends JPanel
+{
+    // Variables ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // References
+    protected WindowFrame windowFrame;
+    private Graphics2D currentG = null;
+    // Primitive
+    protected int width;
+    protected int height;
+    protected boolean locked = false;
+    // Constructors ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Canvas(WindowFrame windowFrame, int givenSizeX, int givenSizeY)
+    {
+        this.windowFrame = windowFrame;
+        this.width = givenSizeX;
+        this.height = givenSizeY;
+    }
+    // Override: Get Preferred Size ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public Dimension getPreferredSize()
+    {
+      return new Dimension(width, height);
+    }
+    // Override: paintComponent ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private long lastFrameTime = 0;
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        // lock canvas
+        locked = true;
+        // call super
+        super.paintComponent(g);
+        // calculate sizing
+        width = (int) windowFrame.getContentPane().getSize().getWidth();
+        height = (int) windowFrame.getContentPane().getSize().getHeight();
+        windowFrame.window.width = width;
+        windowFrame.window.height = height;
+        // get Graphics2D
+        currentG = (Graphics2D) g;
+        // call onFrame-Method of gameloopObject
+        long time = System.nanoTime();
+        windowFrame.window.deltaTime = (double) (time - lastFrameTime) / 1_000_000_000;
+        lastFrameTime = time;
+        windowFrame.window.onFrame();
+        // reset Graphics2D
+        currentG = null;
+        // unlock canvas
+        locked = false;
+    }
+    // Full Draw Methods
+    protected void drawRect(double posX, double posY, double sizeX, double sizeY, int colorA, int colorR, int colorG, int colorB)
+    {
+        if(sizeX == -1) sizeX = width;
+        if(sizeY == -1) sizeY = height;
+        currentG.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) colorA / 255));
+        currentG.setPaint(new Color(colorR, colorG, colorB));
+        currentG.fillRect((int) Math.floor(posX), (int) Math.floor(posY), (int) Math.floor(sizeX), (int) Math.floor(sizeY));
+    }
+    protected void drawOval(double posX, double posY, double sizeX, double sizeY, int colorA, int colorR, int colorG, int colorB)
+    {
+        currentG.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) colorA / 255));
+        currentG.setPaint(new Color(colorR, colorG, colorB));
+        currentG.fillOval((int) Math.floor(posX), (int) Math.floor(posY), (int) Math.floor(sizeX), (int) Math.floor(sizeY));
+    }
+    protected void drawSprite(Sprite sprite, double posX, double posY, double sizeX, double sizeY, int srcPosX, int srcPosY, int srcSizeX, int srcSizeY, int colorA)
+    {
+        currentG.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) colorA / 255));
+        currentG.drawImage(sprite.getImage(), (int) Math.floor(posX), (int) Math.floor(posY), (int) Math.floor(sizeX), (int) Math.floor(sizeY), srcPosX, srcPosY, srcSizeX, srcSizeY, null);
+    }
+    protected void drawText(String content, double posX, double posY, int positioning, double fontSize, String fontFamily, int colorA, int colorR, int colorG, int colorB)
+    {
+        currentG.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) colorA / 255));
+        currentG.setPaint(new Color(colorR, colorG, colorB));
+        Font font = new Font(fontFamily, Font.PLAIN, (int) Math.floor(fontSize));
+        currentG.setFont(font);
+        double newX = posX;
+        switch(positioning)
+        {
+            case TextPositioning.LEFT:
+                newX -= currentG.getFontMetrics(font).stringWidth(content);
+                break;
+            case TextPositioning.CENTER:
+                newX -= (currentG.getFontMetrics(font).stringWidth(content) / 2.0);
+                break;
+        }
+        currentG.drawString(content, (int) Math.floor(newX), (int) Math.floor(posY));
+    }
+    protected void drawLine(double posX, double posY, double endX, double endY, double lineWidth, int colorA, int colorR, int colorG, int colorB)
+    {
+        currentG.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) colorA / 255));
+        currentG.setPaint(new Color(colorR, colorG, colorB));
+        currentG.setStroke(new BasicStroke((float) Math.floor(lineWidth)));
+        currentG.drawLine((int) Math.floor(posX), (int) Math.floor(posY), (int) Math.floor(endX), (int) Math.floor(endY));
+    }
+}
---
+++ devtaube/wein2d/Wein2DApplication.java
@@ -0,0 +1,217 @@
+package devtaube.wein2d;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+
+public abstract class Wein2DApplication
+{
+    // Variables ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // References
+    private WindowFrame jFrame;
+    // primitives
+    public int width = 0;
+    public int height = 0;
+    public double deltaTime = 0;
+    // Constructor and setup ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private int sizeX;
+    private int sizeY;
+    private boolean fullscreen;
+    private String title;
+    private boolean resizable;
+    private int fps;
+    private Image icon;
+    private boolean doHardwareAcceleration;
+
+    public Wein2DApplication()
+    {
+        this.sizeX = 848;
+        this.sizeY = 480;
+        this.fullscreen = false;
+        this.title = "Wein2D Application";
+        this.resizable = true;
+        this.fps = 60;
+        this.icon = null;
+        this.doHardwareAcceleration = true;
+    }
+
+    public final void setSize(int sizeX, int sizeY)
+    {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+    }
+
+    public final void setFullscreen(boolean fullscreen)
+    {
+        this.fullscreen = fullscreen;
+    }
+
+    public final void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public final void setResizable(boolean resizable)
+    {
+        this.resizable = resizable;
+    }
+
+    public final void setTargetedFPS(int fps)
+    {
+        this.fps = fps;
+    }
+
+    public final void setIcon(String iconPath) {
+        try
+        {
+            String fullIconPath = System.getProperty("user.dir") + "/" + iconPath;
+            fullIconPath = fullIconPath.replace("\\", "/");
+            if (!(new File(fullIconPath).isFile()))
+                throw new IllegalArgumentException("Image file at '" + fullIconPath + "' not found.");
+            this.icon = ImageIO.read(new File(fullIconPath));
+        }
+        catch(Exception e) {}
+    }
+
+    public final void setIcon(Image image)
+    {
+        this.icon = image;
+    }
+
+    public final void setHardwareAcceleration(boolean doHardwareAcceleration)
+    {
+        this.doHardwareAcceleration = doHardwareAcceleration;
+    }
+
+    public final void build()
+    {
+        if(doHardwareAcceleration)
+        {
+            System.setProperty("sun.java2d.transaccel", "True");
+            System.setProperty("sun.java2d.d3d", "True");
+            System.setProperty("sun.java2d.ddforcevram", "True");
+            System.setProperty("sun.java2d.opengl", "True");
+        }
+        if(jFrame != null)
+        {
+            if(jFrame.gameloopThread != null) jFrame.gameloopThread.running = false;
+            jFrame.dispose();
+        }
+        // build window and set width and height variables
+        jFrame = new WindowFrame(this, sizeX, sizeY, fullscreen, title, resizable, fps, icon);
+    }
+
+    // Methods ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public int getFPS()
+    {
+        if(jFrame != null)
+        {
+            if(jFrame.gameloopThread != null)
+                return jFrame.gameloopThread.getFPS();
+        }
+        return -1;
+    }
+
+    // Input ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public boolean getKey(int key) {
+        return jFrame.keyAdapter.keys[key];
+    }
+    public int getMouseX()
+    {
+      return jFrame.mouseAdapter.mouseX;
+    }
+    public int getMouseY()
+    {
+      return jFrame.mouseAdapter.mouseY;
+    }
+    public boolean getMouseL()
+    {
+      return jFrame.mouseAdapter.mouseButtonL;
+    }
+    public boolean getMouseR()
+    {
+      return jFrame.mouseAdapter.mouseButtonR;
+    }
+    // draw ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public abstract void onFrame();
+
+    public void drawRect(double posX, double posY, double sizeX, double sizeY, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawRect(posX, posY, sizeX, sizeY, 255, colorR, colorG, colorB);
+    }
+    public void drawRect(double posX, double posY, double sizeX, double sizeY, int colorA, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawRect(posX, posY, sizeX, sizeY, colorA, colorR, colorG, colorB);
+    }
+
+    public void drawOval(double posX, double posY, double sizeX, double sizeY, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawOval(posX, posY, sizeX, sizeY, 255, colorR, colorG, colorB);
+    }
+    public void drawOval(double posX, double posY, double sizeX, double sizeY, int colorA, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawOval(posX, posY, sizeX, sizeY, colorA, colorR, colorG, colorB);
+    }
+
+    public void drawSprite(Sprite sprite, double posX, double posY)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawSprite(sprite, posX, posY, sprite.getImage().getWidth(null) + posX, sprite.getImage().getHeight(null) + posY, 0, 0, sprite.getImage().getWidth(null), sprite.getImage().getHeight(null), 255);
+    }
+    public void drawSprite(Sprite sprite, double posX, double posY, int colorA)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawSprite(sprite, posX, posY, sprite.getImage().getWidth(null) + posX, sprite.getImage().getHeight(null) + posY, 0, 0, sprite.getImage().getWidth(null), sprite.getImage().getHeight(null), colorA);
+    }
+    public void drawSprite(Sprite sprite, double posX, double posY, double sizeX, double sizeY)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawSprite(sprite, posX, posY, sizeX + posX, sizeY + posY, 0, 0, sprite.getImage().getWidth(null), sprite.getImage().getHeight(null), 255);
+    }
+    public void drawSprite(Sprite sprite, double posX, double posY, double sizeX, double sizeY, int colorA)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawSprite(sprite, posX, posY, sizeX + posX, sizeY + posY, 0, 0, sprite.getImage().getWidth(null), sprite.getImage().getHeight(null), colorA);
+    }
+    public void drawSprite(Sprite sprite, double posX, double posY, double sizeX, double sizeY, int srcPosX, int srcPosY, int srcSizeX, int srcSizeY)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawSprite(sprite, posX, posY, sizeX + posX, sizeY + posY, srcPosX, srcPosY, srcSizeX + srcPosX, srcSizeY + srcPosY, 255);
+    }
+    public void drawSprite(Sprite sprite, double posX, double posY, double sizeX, double sizeY, int srcPosX, int srcPosY, int srcSizeX, int srcSizeY, int colorA)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawSprite(sprite, posX, posY, sizeX + posX, sizeY + posY, srcPosX, srcPosY, srcSizeX + srcPosX, srcSizeY + srcPosY, colorA);
+    }
+
+    public void drawText(String content, double posX, double posY, double fontSize, String fontFamily, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawText(content, posX, posY + fontSize, TextPositioning.RIGHT, fontSize, fontFamily, 255, colorR, colorG, colorB);
+    }
+    public void drawText(String content, double posX, double posY, double fontSize, String fontFamily, int colorA, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawText(content, posX, posY + fontSize, TextPositioning.RIGHT, fontSize, fontFamily, colorA, colorR, colorG, colorB);
+    }
+    public void drawText(String content, double posX, double posY, int positioning, double fontSize, String fontFamily, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawText(content, posX, posY + fontSize, positioning, fontSize, fontFamily, 255, colorR, colorG, colorB);
+    }
+    public void drawText(String content, double posX, double posY, int positioning, double fontSize, String fontFamily, int colorA, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawText(content, posX, posY + fontSize, positioning, fontSize, fontFamily, colorA, colorR, colorG, colorB);
+    }
+
+    public void fill(int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawRect(0, 0, -1, -1, 255, colorR, colorG, colorB);
+    }
+    public void fill(int colorA, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawRect(0, 0, -1, -1, colorA, colorR, colorG, colorB);
+    }
+
+    public void drawLine(double posX, double posY, double endX, double endY, double lineWidth, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawLine(posX, posY, endX, endY, lineWidth, 255, colorR, colorG, colorB);
+    }
+    public void drawLine(double posX, double posY, double endX, double endY, double lineWidth, int colorA, int colorR, int colorG, int colorB)
+    {
+        if(jFrame.canvas.locked) jFrame.canvas.drawLine(posX, posY, endX, endY, lineWidth, colorA, colorR, colorG, colorB);
+    }
+}
