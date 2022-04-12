8866d8d792693805cb7149d4f37176d176825e1b
DevTaube <78918080+DevTaube@users.noreply.github.com>
2022-04-12 21:19:41 +0300

    Add files via upload

--- devtaube/wein2d/Sound.java
+++ devtaube/wein2d/Sound.java
@@ -1,18 +1,49 @@
+/*
+ * Copyright (c) 2022, DevTaube
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *     Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package devtaube.wein2d;
+
+import kuusisto.tinysound.ModdedTinySound;
+import kuusisto.tinysound.Music;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
 import java.io.File;
 import java.net.URL;
 
 public final class Sound
 {
 
-    private Clip clip;
-    private final FloatControl volumeControl;
-    private final float volumeRange;
+    static
+    {
+        ModdedTinySound.init();
+    }
+
+    private Music music;
+    private boolean loop = false;
 
     public Sound(String filePath, FileOrigin fileOrigin)
     {
@@ -33,45 +64,34 @@ public final class Sound
                     audioInputStream = AudioSystem.getAudioInputStream(new URL(filePath));
                     break;
             }
-            clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
+            music = ModdedTinySound.loadMusic(audioInputStream);
         }
         catch(Exception e) { e.printStackTrace(); }
-        volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-        volumeRange = volumeControl.getMaximum() - volumeControl.getMinimum();
     }
 
     public void setVolume(double volume)
     {
-        volumeControl.setValue((float) (volumeRange * volume) + volumeControl.getMinimum());
+        music.setVolume(volume);
     }
 
     public void play()
     {
-        clip.setFramePosition(0);
-        clip.start();
+        music.play(loop);
     }
 
     public void loop(boolean looping)
     {
-        if(looping)
-        {
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        }
-        else
-        {
-            clip.loop(0);
-        }
+        loop = looping;
     }
 
     public void stop()
     {
-        clip.stop();
+        music.stop();
     }
 
     public boolean isPlaying()
     {
-        return 0 < clip.getFramePosition() && clip.getFramePosition() < clip.getFrameLength();
+        return music.playing();
     }
 
 }
